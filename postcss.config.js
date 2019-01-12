62d4af1a590fc6c72f63c9cc3fd873c74ff7ca0f
SeptimusVII <vansteene.quentin@gmail.com>
2019-01-12 17:34:40 +0100

    UPDATED DEPENDANCIES * updated Font-awesome * updated autoprefixer * updated postcss * updated postcss-loader
    MIXINS
    + added repeat mixin (return a string repeated n times)
    
    CSS GRID
    + added new partial for grid layout
    
    CSS IMAGES
    +added class img-container
    
    CSS FONTS
    + added class ft-nowrap
    
    CSS UTILITIES
    + added classes w-full & w-fit
    * bugfixed separator classes

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
+    require('autoprefixer')({ grid: "no-autoplace" }),
     require('postcss-focus'),
     require('postcss-object-fit-images'),
   ]
