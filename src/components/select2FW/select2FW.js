35dc79f921c5c3fc3e98f444246b8323c0bf5553
SeptimusVII <vansteene.quentin@gmail.com>
2019-07-03 19:25:10 +0200

    select2FW - improvment Select2 can now works in a standalone maner (without jQuery being a global variable)

--- src/components/select2FW/select2FW.js
+++ src/components/select2FW/select2FW.js
@@ -1,14 +1,14 @@
-require('select2');
+var select2 = require('select2');
 var Select2FW = Object.getPrototypeOf(app).Select2FW = new Component("select2FW");
 // Select2FW.debug = true;
 Select2FW.createdAt      = "1.0.0";
-Select2FW.lastUpdate     = "1.4.9";
-Select2FW.version        = "1.1.0";
+Select2FW.lastUpdate     = "1.4.14";
+Select2FW.version        = "1.1.1";
 // Select2FW.loadingMsg     = "This message will display in the console when component will be loaded.";
 
 Select2FW.prototype.onCreate = function(){
   var select2FW = this;
-  select2FW.$el.select2({
+  select2FW.select2 = new select2(select2FW.$el,{
     minimumResultsForSearch: 5,
     width: '100%',
     dropdownParent: select2FW.$el.parent()
@@ -18,7 +18,7 @@ Select2FW.prototype.onCreate = function(){
 
 Select2FW.prototype.onDestroy = function(){
   var select2FW = this;
-  select2FW.$el.select2('destroy');
+  select2FW.select2.destroy();
   if(Select2FW.debug) console.log('Select2FW has been destroyed \n ',select2FW);
 }
 
