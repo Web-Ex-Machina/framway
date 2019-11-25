c3c603599dc22f84c4f05f18014f945141358ac4
Quentin Vansteene <vansteene.quentin@gmail.com>
2019-11-25 10:58:04 +0100

    v1.4.16 add configCSS key to app object improve grid responsive gap

--- src/index.js
+++ src/index.js
@@ -9,6 +9,22 @@ app.debug = false;
 app.loadThemes(config.themes);
 app.loadComponents(config.components);
 
+
+app.configCSS = require('./scss/_config.scss');
+$.each(app.themes,function(index,theme){
+	config = Object.assign(config,require('./themes/'+theme+'/_config.scss'));
+});
+$.each(app.configCSS,function(key,value){
+	if(value[0] == '(' && value[value.length - 1] == ")"){
+	  var objValue = value.replace('(','{').replace(')','}').replace(/ /g, '')
+	          .replace(/([\w]+):/g, '"$1":')
+	          .replace(/:([\w]+)/g, ':"$1"')
+	          .replace(/:#([\w]+)/g, ':"#$1"')
+	          .replace(/:([\d]+)/g, function(m, num) {return ':'+parseFloat(num)})
+	          .replace(/:([[{])/g, ':$1');
+	  app.configCSS[key] = JSON.parse(objValue);
+	}
+});
 $(function(){
   setTimeout(function(){
     if(app.debug){
